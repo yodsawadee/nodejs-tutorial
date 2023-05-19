9461280664ddb5b2b7245ce5d91f17d77f4ab4ea
yodsawadee <jan.yodsawadee@outlook.com>
2023-05-19 09:58:55 +0700

    refactor

--- simple-node-project/config/dbConn.js
+++ simple-node-project/config/dbConn.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
             useNewUrlParser: true
         })
     } catch (err) {
-        console.loglog(err);
+        console.log(err);
     }
 }
 
--- simple-node-project/middleware/verifyJWT.js
+++ simple-node-project/middleware/verifyJWT.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader?.startsWith('Bearer ')) return ReadableByteStreamController.sendStatus(401);
+    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
     // console.log(authHeader); // Bearer token
     const token = authHeader.split(' ')[1];
     jwt.verify(
